ce266f9df997447a9b25b4f7ccd9ec36e30e56ea
Ramkumar <ramskv1018@gmail.com>
2026-04-16 09:28:58 +0530

    Update lab.py

--- lab.py
+++ lab.py
@@ -1,2 +1,13 @@
 import cv2
-image = cv2.imread("")
+image = cv2.imread("sample.jpg")
+if image is None:
+    print("Error in Acquiring image")
+    exit()
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray,(5,5),0)
+edges = cv2.Canny(blur,300,500)
+roi = image[100:300, 150:350]
+cv2.imshow("edges",edges)
+cv2.imshow("roi",roi)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
